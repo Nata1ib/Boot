1a895d8f436aa40c7d99dbb6396aff314f5749ce
Polina Glezova <pglezova@gmail.com>
2023-10-13 15:21:25 +0300

    bot config

---
+++ src/main/java/spbstu/hsai/config/BotConfiguration.java
@@ -0,0 +1,18 @@
+package spbstu.hsai.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import spbstu.hsai.Bot;
+
+@Configuration
+public class BotConfiguration {
+    @Bean
+    public TelegramBotsApi telegramBotsApi(Bot bot) throws TelegramApiException {
+        var api = new TelegramBotsApi(DefaultBotSession.class);
+        api.registerBot(bot);
+        return api;
+    }
+}
